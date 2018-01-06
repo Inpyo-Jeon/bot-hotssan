887167dd0fef62a06f8df11bbc7d184362be3ada
jayden <jayden8837@gmail.com>
2018-01-06 17:23:43 +0900

    [hotssane-001] get -> post

--- src/main/java/io/coinpeeker/bot_hotssan/controller/HotssanController.java
+++ src/main/java/io/coinpeeker/bot_hotssan/controller/HotssanController.java
@@ -3,6 +3,7 @@ package io.coinpeeker.bot_hotssan.controller;
 import io.coinpeeker.bot_hotssan.service.HotssanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class HotssanController {
     @Autowired
     private HotssanService hotssanService;
 
-    @GetMapping("/webhook")
+    @PostMapping("/webhook")
     public String webHook() {
         return hotssanService.getWebhook();
     }
