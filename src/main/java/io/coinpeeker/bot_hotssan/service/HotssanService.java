3990e94259b1c80b1e0bacce031271260bd39e93
jayden <jayden8837@gmail.com>
2018-01-06 16:15:29 +0900

    [hotssane-001] webhook connection test

--- src/main/java/io/coinpeeker/bot_hotssan/service/HotssanService.java
+++ src/main/java/io/coinpeeker/bot_hotssan/service/HotssanService.java
@@ -1,11 +1,16 @@
 package io.coinpeeker.bot_hotssan.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HotssanService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HotssanService.class);
+
     public String getWebhook() {
+        LOGGER.info("@#$@#$@#$ telegram call");
         return "telegram_bot web_hook";
     }
 }
