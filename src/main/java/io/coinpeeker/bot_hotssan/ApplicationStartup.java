3098fd39e3b42aa2439144fba2ab3a7bb27688ee
jayden <jayden8837@gmail.com>
2018-01-12 00:39:43 +0900

    [hotssan-005] onApplicationEvent test

--- src/main/java/io/coinpeeker/bot_hotssan/ApplicationStartup.java
+++ src/main/java/io/coinpeeker/bot_hotssan/ApplicationStartup.java
@@ -1,6 +1,8 @@
 package io.coinpeeker.bot_hotssan;
 
 import io.coinpeeker.bot_hotssan.service.HotssanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -9,11 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationStartup.class);
+
     @Autowired
     private HotssanService hotssanService;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        LOGGER.info("#$#$#$ onApplicationEvent : {}", event);
         hotssanService.sendBootMessage();
 
         return ;
