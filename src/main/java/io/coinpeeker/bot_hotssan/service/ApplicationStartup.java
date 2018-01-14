39a5ffabcc6d915beef3e6aa8ad8323a65195cd9
jayden <jayden8837@gmail.com>
2018-01-14 20:07:55 +0900

    [hotssan-005] test

--- src/main/java/io/coinpeeker/bot_hotssan/service/ApplicationStartup.java
+++ src/main/java/io/coinpeeker/bot_hotssan/service/ApplicationStartup.java
@@ -3,6 +3,8 @@ package io.coinpeeker.bot_hotssan.service;
 import io.coinpeeker.bot_hotssan.common.CommonConstant;
 import io.coinpeeker.bot_hotssan.utils.MessageUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HotssanService.class);
 
     @Autowired
     private MessageUtils messageUtils;
@@ -33,6 +37,7 @@ public class ApplicationStartup implements ApplicationListener<ContextRefreshedE
             chatId = -300048567L;
         }
 
+        LOGGER.info("#$#$#$ 새로운 build : {}, chatId : {}", url, chatId);
         messageUtils.sendMessage(url, chatId, "새로운 Build 가 감지되었습니다.");
     }
 }
