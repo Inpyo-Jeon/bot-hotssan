de4728481f51b8364fb26ff3a733c323f1e05a05
jayden <jayden8837@gmail.com>
2018-01-21 22:39:48 +0900

    [hotssan-010] scheduled 작업 추가

--- src/main/java/io/coinpeeker/bot_hotssan/BotHotssanApplication.java
+++ src/main/java/io/coinpeeker/bot_hotssan/BotHotssanApplication.java
@@ -2,8 +2,10 @@ package io.coinpeeker.bot_hotssan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BotHotssanApplication {
 
 	public static void main(String[] args) {
