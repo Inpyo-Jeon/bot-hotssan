7ebea64d083d41fb07634874668f648fd9030290
In-Pyo <virustaruin@gmail.com>
2018-01-24 00:44:44 +0900

    created about coinType enum class, commander logic.

---
+++ src/main/java/io/coinpeeker/bot_hotssan/model/constant/CoinType.java
@@ -0,0 +1,27 @@
+package io.coinpeeker.bot_hotssan.model.constant;
+
+public enum CoinType {
+
+    BTC("비트코인"),
+    ETH("이더리움"),
+    ETC("이더리움클래식"),
+    DENT("덴트"),
+    MED("메디블록"),
+    TRX("트론"),
+    QTUM("퀀텀"),
+    NEO("네오"),
+    XGOX("곡스"),
+    BCH("비트코인캐시"),
+    XRP("리플");
+
+
+    private String symbol;
+
+    CoinType(String symbol){
+        this.symbol = symbol;
+    }
+    public String getSymbol(){
+        return this.symbol;
+    }
+
+}
--- src/main/java/io/coinpeeker/bot_hotssan/utils/Commander.java
+++ src/main/java/io/coinpeeker/bot_hotssan/utils/Commander.java
@@ -2,6 +2,8 @@ package io.coinpeeker.bot_hotssan.utils;
 
 import io.coinpeeker.bot_hotssan.external.ApiClient;
 import io.coinpeeker.bot_hotssan.model.CoinPrice;
+import io.coinpeeker.bot_hotssan.model.constant.CoinType;
+import org.apache.commons.lang3.EnumUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -24,19 +26,27 @@ public class Commander {
 
     public String execute(String instruction) {
         StringBuilder result = new StringBuilder();
+        String coinSymbol = instruction.replace("/", "").toUpperCase();
 
-        CoinPrice coinPrice = coinrailApiClient.getCoinPrice("trx");
-        result.append("### ");
-        result.append(coinPrice.getKey());
-        result.append(" ###");
-        result.append("\n거래소명 : ");
-        result.append(coinPrice.getExchangeName());
-        result.append("\nkrw : ");
-        result.append(coinPrice.getKrw());
-        result.append("\nusd : ");
-        result.append(coinPrice.getUsd());
-        result.append("\n사토시 : ");
-        result.append(coinPrice.getSatoshi());
+
+        if(EnumUtils.isValidEnum(CoinType.class, coinSymbol)){
+            CoinPrice coinPrice = coinrailApiClient.getCoinPrice(coinSymbol);
+            result.append("### ");
+            result.append(coinPrice.getKey());
+            result.append(" ###");
+            result.append("\n거래소명 : ");
+            result.append(coinPrice.getExchangeName());
+            result.append("\nkrw : ");
+            result.append(coinPrice.getKrw());
+            result.append("\nusd : ");
+            result.append(coinPrice.getUsd());
+            result.append("\n사토시 : ");
+            result.append(coinPrice.getSatoshi());
+        } else {
+            result.append("등록 되어있지 않은 명령어 혹은 심볼입니다.");
+        }
+
+
 
         return result.toString();
     }
