c701f0117812a7f846153a2f709a206fab46f148
In-Pyo <virustaruin@gmail.com>
2018-03-29 23:42:02 +0900

    [dev] redis host modify

--- src/main/java/io/coinpeeker/bot_hotssan/common/CustomJedis.java
+++ src/main/java/io/coinpeeker/bot_hotssan/common/CustomJedis.java
@@ -1,12 +1,19 @@
 package io.coinpeeker.bot_hotssan.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 @Component
 public class CustomJedis {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
 
     private JedisPool jedisPool;
 
