50af175e911e1deffc444c1da5e4226f24507d7e
In-Pyo <virustaruin@gmail.com>
2018-04-06 13:38:11 +0900

    [hotfix] Upbit 임시 중단

--- src/main/java/io/coinpeeker/bot_hotssan/scheduler/listed/UpbitListedScheduler.java
+++ src/main/java/io/coinpeeker/bot_hotssan/scheduler/listed/UpbitListedScheduler.java
@@ -47,7 +47,7 @@ public class UpbitListedScheduler implements Listing {
     private static final Logger LOGGER = LoggerFactory.getLogger(UpbitListedScheduler.class);
 
     @Override
-    @Scheduled(initialDelay = 1000 * 70, fixedDelay = 1000 * 10)
+//    @Scheduled(initialDelay = 1000 * 70, fixedDelay = 1000 * 10)
     public void inspectListedCoin() throws IOException {
         /** env validation check.**/
         if (!StringUtils.equals("real", env)) {
