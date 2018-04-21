a277ec550829c758834489cc09047553a586a1f0
In-Pyo <virustaruin@gmail.com>
2018-04-21 18:03:30 +0900

    [hotssan-049] bithumb(cafe) listed dev test

--- src/main/java/io/coinpeeker/bot_hotssan/scheduler/listed/BithumbListedScheduler.java
+++ src/main/java/io/coinpeeker/bot_hotssan/scheduler/listed/BithumbListedScheduler.java
@@ -8,11 +8,17 @@ import io.coinpeeker.bot_hotssan.utils.MessageUtils;
 import io.coinpeeker.bot_hotssan.utils.bithumb.Api_Client;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 
@@ -46,89 +52,167 @@ public class BithumbListedScheduler implements Listing {
     private static final Logger LOGGER = LoggerFactory.getLogger(BithumbListedScheduler.class);
 
     @Override
-//    @Scheduled(initialDelay = 1000 * 30, fixedDelay = 1000)
-    public void inspectListedCoin() throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+    @Scheduled(initialDelay = 1000 * 5, fixedDelay = 1000 * 2)
+    public void inspectListedCoin() throws IOException {
         /** env validation check.**/
-        if (!StringUtils.equals("real", env)) {
+        if (!StringUtils.equals("dev", env)) {
             return;
         }
-        LOGGER.info("Bithumb 시작");
 
-        List<String> noListedCoinList = new ArrayList<>();
-        List<String> capList = new ArrayList<>();
-        capList.addAll(CommonConstant.getCapList());
+        CloseableHttpResponse httpResponse = httpUtils.get("https://bithumb.cafe/feed");
+        String convertData = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
 
-        capList.stream()
-                .forEach((key) -> {
-                    synchronized (jedis) {
-                        if (!jedis.hexists("L-Bithumb", key)) {
-                            noListedCoinList.add(key);
-                        }
+        Elements items = Jsoup.parseBodyFragment(convertData).body().getElementsByTag("item");
+
+        for (Element item : items) {
+            if (item.getElementsByTag("title").get(0).text().contains("상장")) {
+                String text = item.getElementsByTag("title").get(0).text();
+                String link = item.getElementsByTag("link").get(0).text();
+                String pubDate = item.getElementsByTag("pubDate").get(0).text();
+                boolean check = false;
+
+                synchronized (jedis) {
+                    if (!jedis.hexists("L-Bithumb-Private", text)) {
+                        check = true;
                     }
-                });
-
-        for (String item : noListedCoinList) {
-            Api_Client api = new Api_Client("1dc81ffb56788858690afb7b72bef9dd", "5ed812f9bfdf8fc23fd422742706947a");
-
-            HashMap<String, String> rgParams = new HashMap<>();
-            rgParams.put("currency", item);
-
-            String result = "";
-            JSONObject jsonObject = null;
-
-            try {
-                result = api.callApi("/info/wallet_address", rgParams);
-                jsonObject = new JSONObject(result);
-            } catch (Exception e) {
-                continue;
-            }
-
-            if (jsonObject.has("data")) {
-                String status = jsonObject.getString("status");
-                String currency = jsonObject.getJSONObject("data").getString("currency");
-                Map<String, List<String>> marketList = marketInfo.availableMarketList(currency);
-
-                StringBuilder messageContent = new StringBuilder();
-                Date nowDate = new Date();
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss (z Z)");
-                simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
-
-                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
-                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
-                messageContent.append(" [ Bithumb ] 상장 정보 ");
-                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
-                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
-                messageContent.append("\n");
-                messageContent.append(simpleDateFormat.format(nowDate));
-                messageContent.append("\n확인방법 : Address");
-                messageContent.append("\n코인정보 : ");
-
-                synchronized (jedis) {
-                    messageContent.append(jedis.hget("I-CoinMarketCap", currency));
                 }
 
-                messageContent.append(" (");
-                messageContent.append(currency);
-                messageContent.append(")");
-                messageContent.append("\n구매가능 거래소 : ");
-                messageContent.append(marketInfo.marketInfo(marketList));
+                if (check) {
+                    StringBuilder messageContent = new StringBuilder();
+                    Date nowDate = new Date();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss (z Z)");
+                    simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
 
-                String url = CommonConstant.URL_TELEGRAM_BASE + apiKey + CommonConstant.METHOD_TELEGRAM_SENDMESSAGE;
-                messageUtils.sendMessage(url, -294606763L, messageContent.toString());
+                    messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+                    messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+                    messageContent.append(" [ Bithumb ] 상장 정보 - Test");
+                    messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+                    messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+                    messageContent.append("\n");
+                    messageContent.append(simpleDateFormat.format(nowDate));
+                    messageContent.append("\n확인방법 : Blog-Internal API");
+                    messageContent.append("\n");
+                    messageContent.append(text);
+                    messageContent.append(link);
+                    messageContent.append(pubDate);
 
-                synchronized (jedis) {
-                    jedis.hset("L-Bithumb", currency, "1");
+                    String url = CommonConstant.URL_TELEGRAM_BASE + apiKey + CommonConstant.METHOD_TELEGRAM_SENDMESSAGE;
+                    messageUtils.sendMessage(url, -294606763L, messageContent.toString());
+
+                    LOGGER.info(text + " | " + link + " | " + pubDate);
+
+                    synchronized (jedis) {
+                        jedis.hset("L-Bithumb-Private", text, "0");
+                    }
+
+//                    messageContent.append("\n코인정보 : ");
+//
+//                    synchronized (jedis) {
+//                        messageContent.append(jedis.hget("I-CoinMarketCap", currency));
+//                    }
+//
+//                    messageContent.append(" (");
+//                    messageContent.append(currency);
+//                    messageContent.append(")");
+//                    messageContent.append("\n구매가능 거래소 : ");
+//                    messageContent.append(marketInfo.marketInfo(marketList));
+//
+//                    String url = CommonConstant.URL_TELEGRAM_BASE + apiKey + CommonConstant.METHOD_TELEGRAM_SENDMESSAGE;
+//                    messageUtils.sendMessage(url, -294606763L, messageContent.toString());
+//
+//                    synchronized (jedis) {
+//                        jedis.hset("L-Bithumb", currency, "1");
+//                    }
+//
+//                    LOGGER.info("Bithumb 상장 : " + currency + " (" + simpleDateFormat.format(nowDate).toString() + ")");
                 }
-
-                LOGGER.info("Bithumb 상장 : " + currency + " (" + simpleDateFormat.format(nowDate).toString() + ")");
             }
-
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
         }
     }
+//    @Override
+//    @Scheduled(initialDelay = 1000 * 30, fixedDelay = 1000)
+//    public void inspectListedCoin() throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+//        /** env validation check.**/
+//        if (!StringUtils.equals("real", env)) {
+//            return;
+//        }
+//        LOGGER.info("Bithumb 시작");
+//
+//        List<String> noListedCoinList = new ArrayList<>();
+//        List<String> capList = new ArrayList<>();
+//        capList.addAll(CommonConstant.getCapList());
+//
+//        capList.stream()
+//                .forEach((key) -> {
+//                    synchronized (jedis) {
+//                        if (!jedis.hexists("L-Bithumb", key)) {
+//                            noListedCoinList.add(key);
+//                        }
+//                    }
+//                });
+//
+//        for (String item : noListedCoinList) {
+//            Api_Client api = new Api_Client("1dc81ffb56788858690afb7b72bef9dd", "5ed812f9bfdf8fc23fd422742706947a");
+//
+//            HashMap<String, String> rgParams = new HashMap<>();
+//            rgParams.put("currency", item);
+//
+//            String result = "";
+//            JSONObject jsonObject = null;
+//
+//            try {
+//                result = api.callApi("/info/wallet_address", rgParams);
+//                jsonObject = new JSONObject(result);
+//            } catch (Exception e) {
+//                continue;
+//            }
+//
+//            if (jsonObject.has("data")) {
+//                String status = jsonObject.getString("status");
+//                String currency = jsonObject.getJSONObject("data").getString("currency");
+//                Map<String, List<String>> marketList = marketInfo.availableMarketList(currency);
+//
+//                StringBuilder messageContent = new StringBuilder();
+//                Date nowDate = new Date();
+//                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss (z Z)");
+//                simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+//
+//                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+//                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+//                messageContent.append(" [ Bithumb ] 상장 정보 ");
+//                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+//                messageContent.append(StringEscapeUtils.unescapeJava("\\ud83d\\ude80"));
+//                messageContent.append("\n");
+//                messageContent.append(simpleDateFormat.format(nowDate));
+//                messageContent.append("\n확인방법 : Address");
+//                messageContent.append("\n코인정보 : ");
+//
+//                synchronized (jedis) {
+//                    messageContent.append(jedis.hget("I-CoinMarketCap", currency));
+//                }
+//
+//                messageContent.append(" (");
+//                messageContent.append(currency);
+//                messageContent.append(")");
+//                messageContent.append("\n구매가능 거래소 : ");
+//                messageContent.append(marketInfo.marketInfo(marketList));
+//
+//                String url = CommonConstant.URL_TELEGRAM_BASE + apiKey + CommonConstant.METHOD_TELEGRAM_SENDMESSAGE;
+//                messageUtils.sendMessage(url, -294606763L, messageContent.toString());
+//
+//                synchronized (jedis) {
+//                    jedis.hset("L-Bithumb", currency, "1");
+//                }
+//
+//                LOGGER.info("Bithumb 상장 : " + currency + " (" + simpleDateFormat.format(nowDate).toString() + ")");
+//            }
+//
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//    }
 }
--- src/test/java/io/coinpeeker/bot_hotssan/module/ModuleTest.java
+++ src/test/java/io/coinpeeker/bot_hotssan/module/ModuleTest.java
@@ -2,22 +2,22 @@ package io.coinpeeker.bot_hotssan.module;
 
 import io.coinpeeker.bot_hotssan.utils.HttpUtils;
 import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Timestamp;
 import java.util.*;
 
 @RunWith(SpringRunner.class)
@@ -31,177 +31,32 @@ public class ModuleTest {
     @Value("${property.env}")
     private String env;
 
+    @Autowired
+    HttpUtils httpUtils;
+
     String standardUrl = "https://api.binance.com";
     String url = "";
     String headerApiKey = "X-MBX-APIKEY";
     String headerApiValue = "";
     String secretKey = "";
     List<NameValuePair> header = new ArrayList<>();
+    List<String> list = Arrays.asList("비트코인", "이더리움", "리플", "비트코인 캐시", "이오스", "라이트코인", "모네로", "트론", "대시", "비체인", "이더리움 클래식", "퀀텀", "아이콘", "비트코인 골드", "제트캐시", "미스릴", "엘프");
 
     @Test
-    public void Test() throws IOException, NoSuchAlgorithmException, InvalidKeyException, InterruptedException {
-        Binance binance = new Binance("m6yQuY6E1BscKqlxIHMhqzkSEa7l9vMKWEQTusyzN9Ozslq3k023x3ou6wxWlJGk", "ZR80HbvYPlckwsbEiyMHNT6nu5SHmLZU3TF95n2uqxloLUmSAz4Rd1yEIooPIbXF");
-        Boolean check = true;
+    public void Test() throws IOException, NoSuchAlgorithmException, InvalidKeyException, InterruptedException, ParserConfigurationException {
+        CloseableHttpResponse httpResponse = httpUtils.get("https://bithumb.cafe/feed");
+        String convertData = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
 
+        Elements items = Jsoup.parseBodyFragment(convertData).body().getElementsByTag("item");
 
-        while (check) {
-            BigDecimal myAxisCoinAmount = new BigDecimal(Double.valueOf(binance.getHaveCoinAmount("BTC")));
-            BigDecimal buyCoinMarketPrice = new BigDecimal(Double.valueOf(binance.getCurrentCoinMarketPrice("IOSTBTC")));
-            int buyAmount = (int) (myAxisCoinAmount.doubleValue() / buyCoinMarketPrice.doubleValue());
-            Double minQuantity = Double.valueOf(binance.getQuantityMinOrder("IOSTBTC"));
-
-            if (buyAmount > minQuantity) {
-                binance.sendOrder("IOSTBTC", "BUY", "MARKET", "1");
-            } else {
-                check = false;
+        for (Element item : items) {
+            if (item.getElementsByTag("title").get(0).text().contains("검토보고서")) {
+                System.out.println(item.getElementsByTag("title").get(0).text());
             }
         }
+
+//
     }
 
 
-    public class Binance {
-
-        @Value("${property.hotssan_id}")
-        private String apiKey;
-
-        @Value("${property.env}")
-        private String env;
-
-        String standardUrl = "https://api.binance.com";
-        String url = "";
-        String headerApiKey = "X-MBX-APIKEY";
-        String headerApiValue = "";
-        String secretKey = "";
-        List<NameValuePair> header = new ArrayList<>();
-        HttpUtils httpUtils = new HttpUtils();
-
-
-        public Binance(String headerApiValue, String secretKey) {
-            this.headerApiValue = headerApiValue;
-            this.secretKey = secretKey;
-            header.add(new BasicNameValuePair(this.headerApiKey, this.headerApiValue));
-        }
-
-        public String getHaveCoinAmount(String symbol) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            String free = "";
-            StringBuilder sb = new StringBuilder();
-            sb.append("recvWindow=");
-            sb.append("5000");
-            sb.append("&timestamp=");
-            sb.append(timestamp.getTime());
-
-            JSONObject jsonObject = getRequest("/api/v3/account", sb.toString(), true);
-            JSONArray jsonArray = jsonObject.getJSONArray("balances");
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                if (jsonArray.getJSONObject(i).getString("asset").equals(symbol)) {
-                    free = jsonArray.getJSONObject(i).getString("free");
-                    break;
-                }
-            }
-            return free;
-        }
-
-        public String getCurrentCoinMarketPrice(String symbol) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            String free = "";
-            StringBuilder sb = new StringBuilder();
-            sb.append("symbol=");
-            sb.append(symbol);
-
-            JSONObject jsonObject = getRequest("/api/v3/ticker/price", sb.toString(), false);
-
-            return jsonObject.getString("price");
-        }
-
-        public Double getQuantityMinOrder(String symbol) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
-            Double minQuantity = 0.0;
-            JSONObject jsonObject = getRequest("/api/v1/exchangeInfo", null, false);
-            JSONArray jsonArray = jsonObject.getJSONArray("symbols");
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                if (symbol.equals(jsonArray.getJSONObject(i).getString("symbol"))) {
-                    JSONObject item = jsonArray.getJSONObject(i);
-                    JSONArray filters = item.getJSONArray("filters");
-
-                    for (int j = 0; j < filters.length(); j++) {
-                        if (filters.getJSONObject(j).has("minQty")) {
-                            minQuantity = filters.getJSONObject(j).getDouble("minQty");
-                            break;
-                        }
-                    }
-                }
-            }
-            return minQuantity;
-        }
-
-        public void sendOrder(String symbol, String side, String type, String quantity) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
-            StringBuilder sb = new StringBuilder();
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            sb.append("symbol=");
-            sb.append(symbol);
-            sb.append("&side=");
-            sb.append(side);
-            sb.append("&type=");
-            sb.append(type);
-            sb.append("&quantity=");
-            sb.append(quantity);
-            sb.append("&recvWindow=");
-            sb.append("5000");
-            sb.append("&timestamp=");
-            sb.append(timestamp.getTime());
-
-            postRequest("/api/v3/order", sb.toString());
-        }
-
-        public JSONObject postRequest(String endPoint, String queryString) throws InvalidKeyException, NoSuchAlgorithmException, IOException {
-            StringBuilder sb = new StringBuilder();
-
-            sb.append(this.standardUrl);
-            sb.append(endPoint);
-            sb.append("?");
-            sb.append(queryString);
-            sb.append("&signature=");
-            Mac mac = Mac.getInstance("HMACSHA256");
-            mac.init(new SecretKeySpec(this.secretKey.getBytes(), "HMACSHA256"));
-
-            for (byte byteItem : mac.doFinal(queryString.getBytes())) {
-                sb.append(Integer.toString((byteItem & 0xFF) + 0x100, 16).substring(1));
-            }
-
-            JSONObject jsonObject = httpUtils.getPostResponseByObject(sb.toString(), header, "binanceTrade");
-            System.out.println(jsonObject);
-            return jsonObject;
-
-        }
-
-        public JSONObject getRequest(String endPoint, String queryString, Boolean isSignature) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-            StringBuilder sb = new StringBuilder();
-            JSONObject jsonObject = null;
-
-            sb.append(this.standardUrl);
-            sb.append(endPoint);
-
-            if (queryString != null) {
-                sb.append("?");
-                sb.append(queryString);
-            }
-
-            if (isSignature) {
-                sb.append("&signature=");
-                Mac mac = Mac.getInstance("HMACSHA256");
-                mac.init(new SecretKeySpec(this.secretKey.getBytes(), "HMACSHA256"));
-
-                for (byte byteItem : mac.doFinal(queryString.getBytes())) {
-                    sb.append(Integer.toString((byteItem & 0xFF) + 0x100, 16).substring(1));
-                }
-            }
-
-            jsonObject = httpUtils.getResponseByObject(sb.toString(), header);
-
-            return jsonObject;
-
-        }
-    }
 }
