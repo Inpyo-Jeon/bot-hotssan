bb820bc68bc883df4dce05eb6fcf9a0e7aa299ba
Inpyo-Jeon <inpyo.developer@gmail.com>
2018-11-12 00:34:53 +0900

    Modify annotation

--- src/main/java/io/coinpeeker/bot_hotssan/BotHotssanApplication.java
+++ src/main/java/io/coinpeeker/bot_hotssan/BotHotssanApplication.java
@@ -3,10 +3,12 @@ package io.coinpeeker.bot_hotssan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories("io.coinpeeker.bot_hotssan.repository")
 public class BotHotssanApplication {
 
     public static void main(String[] args) {
